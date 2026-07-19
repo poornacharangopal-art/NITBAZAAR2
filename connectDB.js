03339d9a84e15a737f84b754458c91b96d3c03bf
Poornacharangopal <poornacharangopal@gmail.com>
2026-07-20 00:19:50 +0530

    Update MongoDB connection to use environment variable

--- connectDB.js
+++ connectDB.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
-const connectDB=async()=>{
-    try{
-        await
-        mongoose.connect("mongodb://127.0.0.1:27017/collegeERP");
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB Connected");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
         process.exit(1);
     }
 };
-module.exports=connectDB;
 
+module.exports = connectDB;
